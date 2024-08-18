44c88f2c841ecf21ff14f9818da3ee7ae436c193
wintakorn <akimijisabitoba@gmail.com>
2024-08-18 19:32:52 +0700

    all upgrade

--- index.js
+++ index.js
@@ -23,12 +23,11 @@ app.use(session({
 
 app.get('/', (request, response) => {
     response.render('index')
-   
 })
-// app.get('/Log_in', (request, response) => {
-//     response.render('Log_in')
-   
-// })
+app.get('/webboard/Log_in', (request, response) => {
+    response.render('Log_in')
+})
+
 app.get('/captcha', (request, response) => {
 	let captcha = svgCaptcha.create({size: 5, noise: 3, background: '#fff'})
 	request.session.captcha = captcha.text
